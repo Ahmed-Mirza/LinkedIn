2825fb5c53fc9da84c43fd43b5215818a4d50a42
Ahmed <ahmed_mirza_baig@hotmail.com>
2013-05-13 10:33:02 -0400

    inital commit

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,44 @@
+class SearchesController < ApplicationController
+# note that all instance variable will be acessable files( or maybe more likely there own coresponding views) withing the view folders
+  def index# default home page
+    @searches = Search.all # @ searches is an instance variable which retreives ans contains all of the searhes which have been saved to the database
+  end
+  
+  def show # shows an individual post which has been saved to the database 
+    @search = Search.find(params[:id])# @ search is an instance variable which will contain specific search which will be selected by the user
+  end
+  
+  def new # displys a form to create a search
+    @search = Search.new
+  end
+  
+  def create # saves a search into the database
+    
+    @search = Search.new(params[:search])
+    
+    if @search.save
+      redirect_to index_path, :notice => "Your Search has been Saved"
+    else
+      render "new"
+    end
+  end
+  #***********************************************
+  # Will not be useing edit update or destroy at 
+  # the moment only required to store and be able
+  # to bring up what is being searched
+  #***********************************************  
+  def edit # displays a form to edit a search
+    
+  end
+
+  def update # updates a search in the database after editing it 
+    
+  end
+  
+  def destroy # deleates a searh from the database
+    @search = Search.find(params[:id])
+    @search.destroy
+    redirect_to searches_path,:notice => "Your Search has been Deleted"
+  end
+  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SearchFinal::Application.config.secret_token = 'd9a2da389fd179ea626872d0e83aed28df17e36f90b063c4c5f1c191a275088ac39d06fc899b351699a91f26b02fa6b54d8a115b34f3da4a956f06008bbd7af0'
